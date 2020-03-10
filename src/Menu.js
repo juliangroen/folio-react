00fa20c1c59d56b129116e988c8edc53615a915a
Foolz <akufoolz@gmail.com>
2020-03-10 00:02:46 -0600

    added menuItem onClick

--- src/Menu.js
+++ src/Menu.js
@@ -1,23 +1,46 @@
-import React, { useState } from 'react'
-import MenuItem from './MenuItem';
+import React, { useState } from "react";
+import MenuItem from "./MenuItem";
 
 const Menu = ({ showMenu }) => {
   const [items, setItems] = useState([
-    {title: 'About', isActive: true},
-    {title: 'Portfolio', isActive: false},
-    {title: 'Contact', isActive: false},
+    { text: "About", isActive: true },
+    { text: "Portfolio", isActive: false },
+    { text: "Contact", isActive: false }
   ]);
+
+  const setActive = text => {
+    const newState = [...items];
+    console.log(newState);
+    newState.forEach(obj => {
+      console.log(text);
+      if (obj.text === text || obj.isActive === true) {
+        obj.isActive = !obj.isActive;
+      }
+    });
+    console.log(newState);
+    setItems(newState);
+  };
+
   return (
     <div>
       {showMenu && (
-        <nav className="relative z-20 flex flex-col tracking-wider" id="navMenu">
+        <nav
+          className="relative z-20 flex flex-col tracking-wider"
+          id="navMenu"
+        >
           {items.map(item => {
-            return <MenuItem itemName={item.title} isActive={item.isActive} />
+            return (
+              <MenuItem
+                itemName={item.text}
+                isActive={item.isActive}
+                clickFunction={setActive}
+              />
+            );
           })}
         </nav>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
--- src/MenuItem.js
+++ src/MenuItem.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const MenuItem = ({ itemName, isActive }) => {
+const MenuItem = ({ itemName, isActive, clickFunction }) => {
   return (
-    <button className={`p-4 text-xl font-semibold text-gray-600 ${isActive ? `text-indigo-400 bg-gray-800` : `bg-gray-900`} text-4lg hover:text-indigo-400`}>
+    <button
+      className={`p-4 text-xl font-semibold text-gray-600 ${
+        isActive ? `text-indigo-400 bg-gray-800` : `bg-gray-900`
+      } text-4lg hover:text-indigo-400`}
+      onClick={() => clickFunction(itemName)}
+    >
       {itemName}
     </button>
-  )
-}
+  );
+};
 
-export default MenuItem
+export default MenuItem;
